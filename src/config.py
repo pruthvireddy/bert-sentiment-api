c88dcff9be1b7bd521a9bafcef4e1fb38d2597ad
Pruthvi Reddy Panati <pruthvi5007@gmail.com>
2021-11-28 23:37:18 -0500

    config params

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import transformers
+
+DEVICE = "cuda"
+MAX_LEN = 64
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+TRAIN_WORKER_SIZE = 4
+VALIDATION_WORKER_SIZE = 1
+BERT_PATH = "bert-base-uncased"
+MODEL_PATH = "/root/docker_data/model.bin"
+TRAINING_FILE = "/root/docker_data/train.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
